12eb783c28910a69ce1b680ce34f66efc2ed80c5
Alicia <marya@192.168.43.177>
2018-06-19 16:53:15 -0300

    hago mi primer commit

--- isw_tp3_git/src/isw_tp3_git/Isw_tp3_git.java
+++ isw_tp3_git/src/isw_tp3_git/Isw_tp3_git.java
@@ -3,6 +3,7 @@ public class Isw_tp3_git {
 
     public static void main(String[] args) {
         System.out.println("Primer línea modificada");
+        System.out.println("Segunda");
     }
     
 }
