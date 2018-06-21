9ae76dc16912afb6db3e22e0917a7a666c9531b0
jero10 <Usuario@jero-hp>
2018-06-21 00:41:11 -0300

    manso cambio

---
+++ src/branch/clase1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package branch;
+
+/**
+ *
+ * @author marya
+ */
+public class clase1 {
+  public int dividemal (){
+    int a=0;
+  int b=0; 
+  return a/b;
+}
+    
+}
