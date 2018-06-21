d877d123f9115bada8dd43639ac5b8f3491a076d
Cyntia <Cyntia@Cyntia-Note.home>
2018-06-21 01:13:18 -0300

    agregue clase producto

---
+++ isw_tp3_git/src/isw_tp3_git/Producto.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package isw_tp3_git;
+
+/**
+ *
+ * @author Cyntia
+ */
+public class Producto {
+    int codigo;
+    String nombrre;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombrre() {
+        return nombrre;
+    }
+
+    public void setNombrre(String nombrre) {
+        this.nombrre = nombrre;
+    }
+    
+}
